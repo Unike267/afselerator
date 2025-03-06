87e7c5e84fa623d7ebb9aee57a777bd4aa997b54
Unai Sainz-Estebanez <unike267@gmail.com>
2025-03-06 16:43:02 +0100

    Update CI in order to add neorv32 ROM wishbone simulation

---
+++ sim/MEM/ROM/neorv32/neorv32_ROM_wishbone_alone_sim-build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -ex
+
+cd $(dirname "$0")
+
+cd neorv32-setups/neorv32/sw/example 
+mkdir rom-partial 
+mkdir rom-all 
+cp hello_world/makefile rom-partial/makefile 
+cp hello_world/makefile rom-all/makefile 
+cp ../../../../sw/MEM/ROM/main.c rom-partial/main.c 
+cp ../../../../sw/MEM/ROM/main.c rom-all/main.c 
+cd rom-partial
+sed -i '19s|//#define sim|#define sim|' main.c
+cd ../rom-all
+sed -i '19s|//#define sim|#define sim|' main.c
+sed -i '51s|  uint32_t partial = 1;|  uint32_t partial = 0;|' main.c
+cd ../../..
+make -C sw/example/rom-partial clean_all MARCH=rv32imac_zicsr_zifencei info image
+make -C sw/example/rom-all clean_all MARCH=rv32imac_zicsr_zifencei info image
+cp sw/example/rom-partial/neorv32_application_image.vhd ../../partial_neorv32_application_image.vhd
+cp sw/example/rom-all/neorv32_application_image.vhd ../../all_neorv32_application_image.vhd
---
+++ sim/MEM/ROM/neorv32/neorv32_ROM_wishbone_alone_sim-run.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -ex
+
+cd $(dirname "$0")
+
+mv partial_neorv32_application_image.vhd neorv32-setups/neorv32/rtl/core/neorv32_application_image.vhd
+cd sim/MEM/ROM/neorv32
+./run.py -v MEM.tb_neorv32_rom_wishbone.8.partial_test  --gtkwave-fmt vcd
+./run.py -v MEM.tb_neorv32_rom_wishbone.16.partial_test --gtkwave-fmt vcd
+./run.py -v MEM.tb_neorv32_rom_wishbone.32.partial_test --gtkwave-fmt vcd
+mv ../../../../all_neorv32_application_image.vhd ../../../../neorv32-setups/neorv32/rtl/core/neorv32_application_image.vhd
+./run.py -v MEM.tb_neorv32_rom_wishbone.8.read_all_test  --gtkwave-fmt vcd
+./run.py -v MEM.tb_neorv32_rom_wishbone.16.read_all_test 
+./run.py -v MEM.tb_neorv32_rom_wishbone.32.read_all_test 
+mv vunit_out/wave/*.vcd ../../../..
+mv vunit_out/outcsv/*.csv ../../../..
