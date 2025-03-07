210ac63d58edd77bc466babccce597d47d6b4104
Unai Sainz-Estebanez <unike267@gmail.com>
2025-03-07 10:04:26 +0100

    Cleanup ROM RTL file

--- rtl/MEM/ROM/ROM.vhd
+++ rtl/MEM/ROM/ROM.vhd
@@ -23,9 +23,9 @@ entity ROM is
     ROM_LOAD_FILE : string   -- ROM_LOAD_FILE = PATH of the LOAD FILE
   );
   port (
-    CLK   : in  std_logic; -- Clock
+    CLK   : in  std_logic;                              -- Clock
     ADDR  : in  std_logic_vector(ROM_DEPTH-1 downto 0); -- ROM address
-    STB   : in  std_logic; -- ROM strobe signal
+    STB   : in  std_logic;                              -- ROM strobe signal
     DOUT  : out std_logic_vector(ROM_WIDTH-1 downto 0)  -- ROM output data
   );
 end ROM;
@@ -36,49 +36,49 @@ architecture ROM_RTL of ROM is
   constant ROM_LOAD_FILE_16 : string := ROM_LOAD_FILE & "string_16.hex"; 
   constant ROM_LOAD_FILE_32 : string := ROM_LOAD_FILE & "string_32.hex"; 
 
-  constant MEM_8  : MEM8_t (0 to ( 2**ROM_DEPTH)-1) := MEM8_INIT_HEX (ROM_LOAD_FILE_8,  2**ROM_DEPTH);
-  constant MEM_16 : MEM16_t(0 to ( 2**ROM_DEPTH)-1) := MEM16_INIT_HEX(ROM_LOAD_FILE_16, 2**ROM_DEPTH);
-  constant MEM_32 : MEM32_t(0 to ( 2**ROM_DEPTH)-1) := MEM32_INIT_HEX(ROM_LOAD_FILE_32, 2**ROM_DEPTH);
+  constant ROM_8  : MEM8_t (0 to ( 2**ROM_DEPTH)-1) := MEM8_INIT_HEX (ROM_LOAD_FILE_8,  2**ROM_DEPTH);
+  constant ROM_16 : MEM16_t(0 to ( 2**ROM_DEPTH)-1) := MEM16_INIT_HEX(ROM_LOAD_FILE_16, 2**ROM_DEPTH);
+  constant ROM_32 : MEM32_t(0 to ( 2**ROM_DEPTH)-1) := MEM32_INIT_HEX(ROM_LOAD_FILE_32, 2**ROM_DEPTH);
 
 begin
 
-  MEM_WIDTH_8:
+  ROM_WIDTH_8:
   if (ROM_WIDTH = 8) generate
-  MEM_8_ACCESS:
+  ROM_8_ACCESS:
     process(CLK)
     begin
       if rising_edge(CLK) then 
         if(STB='1') then
-          DOUT <= MEM_8(to_integer(unsigned(ADDR(ROM_DEPTH-1 downto 0))));
+          DOUT <= ROM_8(to_integer(unsigned(ADDR(ROM_DEPTH-1 downto 0))));
         end if;
       end if;
-    end process MEM_8_ACCESS;
+    end process ROM_8_ACCESS;
   end generate;
 
-  MEM_WIDTH_16:
+  ROM_WIDTH_16:
   if (ROM_WIDTH = 16) generate
-  MEM_16_ACCESS:
+  ROM_16_ACCESS:
     process(CLK)
     begin
       if rising_edge(CLK) then 
         if(STB='1') then
-          DOUT <= MEM_16(to_integer(unsigned(ADDR(ROM_DEPTH-1 downto 0))));
+          DOUT <= ROM_16(to_integer(unsigned(ADDR(ROM_DEPTH-1 downto 0))));
         end if;
       end if;
-    end process MEM_16_ACCESS;
+    end process ROM_16_ACCESS;
   end generate;
 
-  MEM_WIDTH_32:
+  ROM_WIDTH_32:
   if (ROM_WIDTH = 32) generate
-  MEM_32_ACCESS:
+  ROM_32_ACCESS:
     process(CLK)
     begin
       if rising_edge(CLK) then 
         if(STB='1') then
-          DOUT <= MEM_32(to_integer(unsigned(ADDR(ROM_DEPTH-1 downto 0))));
+          DOUT <= ROM_32(to_integer(unsigned(ADDR(ROM_DEPTH-1 downto 0))));
         end if;
       end if;
-    end process MEM_32_ACCESS;
+    end process ROM_32_ACCESS;
   end generate;
 
 end ROM_RTL;
