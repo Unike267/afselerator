8ca28c4aa9bce06a04ece5d6d2b2e35cbb414836
Unai Sainz-Estebanez <unike267@gmail.com>
2025-03-13 10:07:10 +0100

    Update MEM_pkg in order to add store functions

--- rtl/MEM/MEM_package.vhd
+++ rtl/MEM/MEM_package.vhd
@@ -21,9 +21,13 @@ package MEM_package is
   type t_MEM16 is array (natural range <>) of std_logic_vector(15 downto 0); -- Memory with 16-bit data width
   type t_MEM32 is array (natural range <>) of std_logic_vector(31 downto 0); -- Memory with 32-bit data width
 
-  impure function MEM8_INIT_HEX (LOAD_FILE : string; MEM_ITEMS : natural) return  t_MEM8;
+  impure function MEM8_INIT_HEX (LOAD_FILE : string; MEM_ITEMS : natural) return t_MEM8;
   impure function MEM16_INIT_HEX(LOAD_FILE : string; MEM_ITEMS : natural) return t_MEM16;
   impure function MEM32_INIT_HEX(LOAD_FILE : string; MEM_ITEMS : natural) return t_MEM32;
+
+  impure function MEM8_STORE_HEX  (STORE_FILE : string; MEM_DEPTH : natural; DIN :  t_MEM8) return boolean;
+  impure function MEM16_STORE_HEX (STORE_FILE : string; MEM_DEPTH : natural; DIN : t_MEM16) return boolean;
+  impure function MEM32_STORE_HEX (STORE_FILE : string; MEM_DEPTH : natural; DIN : t_MEM32) return boolean;
 
 end MEM_package;
 
@@ -45,10 +49,10 @@ package body MEM_package is
   end function;
 
   impure function MEM16_INIT_HEX(LOAD_FILE : STRING; MEM_ITEMS : natural) return t_MEM16 is
-    file     mem_file : text open read_mode is LOAD_FILE;
-    variable mem_line : line;
+    file     mem_file  : text open read_mode is LOAD_FILE;
+    variable mem_line  : line;
     variable temp_word : std_logic_vector(15 downto 0);
-    variable temp_mem : t_MEM16(0 to MEM_ITEMS-1) := (others => (others => '0'));
+    variable temp_mem  : t_MEM16(0 to MEM_ITEMS-1) := (others => (others => '0'));
   begin
     for i in 0 to MEM_ITEMS-1 loop
       exit when endfile (mem_file);
@@ -60,10 +64,10 @@ package body MEM_package is
   end function;
 
   impure function MEM32_INIT_HEX(LOAD_FILE : STRING; MEM_ITEMS : natural) return t_MEM32 is
-    file     mem_file : text open read_mode is LOAD_FILE;
-    variable mem_line : line;
+    file     mem_file  : text open read_mode is LOAD_FILE;
+    variable mem_line  : line;
     variable temp_word : std_logic_vector(31 downto 0);
-    variable temp_mem : t_MEM32(0 to MEM_ITEMS-1) := (others => (others => '0'));
+    variable temp_mem  : t_MEM32(0 to MEM_ITEMS-1) := (others => (others => '0'));
   begin
     for i in 0 to MEM_ITEMS-1 loop
       exit when endfile (mem_file);
@@ -72,6 +76,39 @@ package body MEM_package is
       temp_mem(i)     := temp_word;
     end loop;
     return temp_mem;
+  end function;
+
+  impure function MEM8_STORE_HEX(STORE_FILE : STRING; MEM_DEPTH : natural; DIN : t_MEM8) return boolean is 
+    file     mem_file  : text open write_mode is STORE_FILE;
+    variable mem_line  : line;
+  begin
+    for i in 0 to 2**MEM_DEPTH-1 loop
+      hwrite   (mem_line,   DIN(i));
+      writeline(mem_file, mem_line);
+    end loop;
+    return true;
+  end function;
+
+  impure function MEM16_STORE_HEX(STORE_FILE : STRING; MEM_DEPTH : natural; DIN : t_MEM16) return boolean is 
+    file     mem_file  : text open write_mode is STORE_FILE;
+    variable mem_line  : line;
+  begin
+    for i in 0 to 2**MEM_DEPTH-1 loop
+      hwrite   (mem_line,   DIN(i));
+      writeline(mem_file, mem_line);
+    end loop;
+    return true;
+  end function;
+
+  impure function MEM32_STORE_HEX(STORE_FILE : STRING; MEM_DEPTH : natural; DIN : t_MEM32) return boolean is 
+    file     mem_file  : text open write_mode is STORE_FILE;
+    variable mem_line  : line;
+  begin
+    for i in 0 to 2**MEM_DEPTH-1 loop
+      hwrite   (mem_line,   DIN(i));
+      writeline(mem_file, mem_line);
+    end loop;
+    return true;
   end function;
 
 end MEM_package;
