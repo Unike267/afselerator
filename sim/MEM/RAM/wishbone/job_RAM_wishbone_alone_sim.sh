ec65c35824bd9c680aa05d9b41496b43c1d69c19
Unai Sainz-Estebanez <unike267@gmail.com>
2025-03-14 13:17:24 +0100

    Update CI in order to add RAM wishbone alone simulation

---
+++ sim/MEM/RAM/wishbone/job_RAM_wishbone_alone_sim.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -ex
+
+cd $(dirname "$0")
+
+./run.py -v --gtkwave-fmt vcd
+#./run.py MEM.tb_ram_wishbone.8.test_ram -v --gtkwave-fmt vcd
+
+mv vunit_out/wave/*.vcd   ../../../..
+mv vunit_out/outcsv/*.csv ../../../..
+mv ../../../../data/RAM-sim/*.hex ../../../..
